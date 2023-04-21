d67e227e985f942891bbc1da4d706ced97a24583
Sharonkings <sharonebube2016@gmail.com>
2023-04-21 10:23:21 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char z;
+
+	for (z = '0'; z <= '9'; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
