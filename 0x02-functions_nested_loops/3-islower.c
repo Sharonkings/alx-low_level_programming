5459ff29a3f4ab7bf3c8ab576cc372f101625fe5
Sharonkings <sharonebube2016@gmail.com>
2023-04-25 07:42:24 -0700

    is-lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,16 @@
 /**
  * _islower-check if a character is lowercase.
  * @c:character to check if it islowercase.
- * Return:0-if lowercase,1-if not.
+ * Return:1-if lowercase,0-if not.
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c >= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
