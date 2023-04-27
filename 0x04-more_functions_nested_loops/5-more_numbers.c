f2662c8121694af4408d0df1debc41395845e803
Sharonkings <sharonebube2016@gmail.com>
2023-04-27 08:30:29 -0700

    prints 1-14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ * return : 0
+ */
+
+void more_numbers(void)
+{
+	char n, m;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar('1');
+			_putchar(n % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
+
