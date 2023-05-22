de1de523b128db3750ba1a1cb1157e64cffa2179
Sharonkings <sharonebube2016@gmail.com>
2023-05-22 11:13:39 -0700

    lastone

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /**
- * free_dog - Frees dogs
- * @d: Pointer to dog_t struct
- *
- */
+* free_dog - Frees dogs
+* @d: Pointer to dog_t struct
+*
+*/
 
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-        return;
+if (d == NULL)
+return;
 
-    free(d->name);
-    free(d->owner);
-    free(d);
+free(d->name);
+free(d->owner);
+free(d);
 }
 
