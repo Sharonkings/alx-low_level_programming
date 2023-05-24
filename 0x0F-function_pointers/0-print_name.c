ab100056f04b84874746f0b8521a546e9584b57c
Sharonkings <sharonebube2016@gmail.com>
2023-05-24 04:54:07 -0700

    you

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
  * print_name - prints a name
  * @name: a variable pointing to a character array...the name to print
  * @f: a function pointer that takes a character array
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name == NULL && f == NULL)
+	return;
 	f(name);
 }
